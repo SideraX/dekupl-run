a6bdf1032f7e09d94e2686921475022466a1a1da
Dimitri Larue <contact@dimitrilarue.fr>
2018-10-12 15:00:48 +0200

    refactor: travis conda build

--- scripts/conda.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget https://repo.continuum.io/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh
-bash Miniconda3-4.5.11-Linux-x86_64.sh
-
-conda config --add channels conda-forge
-conda config --add channels bioconda
-
-conda build recipe
